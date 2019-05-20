adee87393e6ea1106b1423a671dd452493051490
Nicola Musolino <nicola@MBP-di-Nicola.station>
2019-05-20 23:32:09 +0200

    medium seo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 class ApplicationController < ActionController::Base
-	before_action :add_view
+	before_action :add_view, :set_title_and_description
 
 	def add_view
 		siteview=Siteview.find_or_initialize_by(:ip => request.remote_ip.to_s)
 		siteview.save
 		@visualizzazioni = Siteview.count
+	end
+
+	def set_title_and_description
+		@title = "Piccoli Diavoli"
+		@description = "Piccoli Diavoli 3 Ruote è una ASD presente nel circuito paraciclistico"
 	end
 
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,43 @@
 class HomeController < ApplicationController
 
 	def index
-
+		@title = "Piccoli Diavoli"
+		@description = "Piccoli Diavoli 3 Ruote è una ASD presente nel circuito paraciclistico"
 	end
 
 	def team
-		
+		@title = "Piccoli Diavoli - Team"
+		@description = "Il Piccoli Diavoli 3 Ruote ruote è composta da fortissimi atleti"
+	end
+
+	def alessandro
+		@title = "Piccoli Diavoli - Alessandro Villa"
+		@description = "Alessandro Villa, detto il boss, creatore della associazione"
+	end
+
+	def andrea
+		@title = "Piccoli Diavoli - Andrea Villa"
+		@description = "Andrea Villa, detto mini Zanardi, il più giovane atleta dei piccoli diavoli 3 ruote è composta da fortissimi atleti"
+	end
+
+	def federico
+		@title = "Piccoli Diavoli - Federico Villa"
+		@description = "Federico Villa, uno dei migliori atleti della Piccoli Diavoli 3 Route"
+	end
+
+	def iago
+		@title = "Piccoli Diavoli - Iago Bernardi"
+		@description = "Iago Bernardi, uno dei migliori atleti della Piccoli Diavoli 3 Route"
+	end
+
+	def franco
+		@title = "Piccoli Diavoli - Franco Manusé"
+		@description = "Franco Manusé, uno dei migliori atleti della Piccoli Diavoli 3 Route"
+	end
+
+	def tempio
+		@title = "Monza,Tempio della velocità - Piccoli Diavoli"
+		@description = "Iago Bernardi, uno dei migliori atleti della Piccoli Diavoli 3 Route"
 	end
 
 end
