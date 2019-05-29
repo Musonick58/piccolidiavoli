c666ca5e38218415e6bc3f946b93a1d27f0c809b
Nicola Musolino <nik894@gmail.com>
2019-05-29 23:19:11 +0200

    prova https

---
+++ app/models/Service/ftp_service.rb
@@ -0,0 +1,72 @@
+class Service::FtpService
+
+  attr_accessor :ftp
+
+  def initialize()
+    @ftp = Net::FTP.new(::DEFAULT_FTP_HOST,::DEFAULT_FTP_USER,::DEFAULT_FTP_PASS)
+    @ftp.passive = true
+  end
+
+  #upload(file,path) 
+  #path = "public/ogimage.jpeg"
+  #file = File.open(path)
+  #ftp = Service::FtpService.new
+  #ftp.upload(file,path)
+  def upload(file,path) 
+    folder = path.split("/")
+    filename = folder[-1]
+    folder[-1] = nil
+    folder = folder.compact
+    folder.each{|folder|
+      @ftp.mkdir(folder) unless self.exist?(folder)
+      @ftp.chdir(folder)
+    }
+    @ftp.putbinaryfile(file)
+    status = self.exist?(filename)
+    @ftp.chdir("/")
+    return status
+  end
+
+  #
+  #path = "public/ogimage.jpeg"
+  #ftp = Service::FtpService.new
+  #ftp.download(path)
+  def download(key)
+    folder = key.split("/")
+    filename = folder[-1]
+    folder[-1] = nil
+    folder = folder.compact
+    folder.each{|folder|
+      @ftp.chdir(folder) if self.exist?(folder)
+      raise "this folder doesn't exist: #{folder}" unless self.exist?(folder)
+    }
+    file = @ftp.getbinaryfile(filename,nil)
+    @ftp.chdir("/")
+    return file
+  end
+
+  def delete(key)
+    folder = key.split("/")
+    filename = folder[-1]
+    folder[-1] = nil
+    folder = folder.compact
+    folder.each{|folder|
+      @ftp.chdir(folder) #if self.exist?(folder)
+    }
+    begin
+      @ftp.delete(filename) 
+    rescue
+      @ftp.chdir("/")
+      return false
+    end
+    @ftp.chdir("/")
+    return true
+  end
+
+  def exist?(key)
+    return false if @ftp.list(key).empty?
+    return true
+  end
+
+end
+
---
+++ config/initializers/ftp_config.rb
@@ -0,0 +1,5 @@
+require "net/ftp"
+DEFAULT_FTP_HOST = "ftp.fumatix.altervista.org"
+DEFAULT_FTP_USER = "fumatix"
+DEFAULT_FTP_PASS = "ckf5QYpUC2gA"
+DEFAULT_FTP_PORT = "21"
