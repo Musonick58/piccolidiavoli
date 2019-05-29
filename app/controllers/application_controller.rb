6384728ce533dd31ee7309e23a21b6d519eb7cc9
Nicola Musolino <nik894@gmail.com>
2019-05-29 23:33:59 +0200

    redirezione a https per ogni link

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,14 @@
 class ApplicationController < ActionController::Base
-	before_action :add_view, :set_title_and_description
+	before_action :https, :add_view, :set_title_and_description
 
 	def add_view
 		siteview=Siteview.find_or_initialize_by(:ip => request.remote_ip.to_s)
 		siteview.save
 		@visualizzazioni = Siteview.count
+	end
+
+	def https
+		return redirect_to(request.original_url.gsub("http://","https://"), :status => 301) if request.original_url["http://"]
 	end
 
 	def set_title_and_description
