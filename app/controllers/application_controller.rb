7bb9c4899680494b23d64477caf14eb34cd64f2c
Nicola Musolino <nik894@gmail.com>
2019-05-30 00:03:58 +0200

    rollback

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,10 @@
 class ApplicationController < ActionController::Base
-	before_action :https, :add_view, :set_title_and_description
+	before_action :add_view, :set_title_and_description
 
 	def add_view
 		siteview=Siteview.find_or_initialize_by(:ip => request.remote_ip.to_s)
 		siteview.save
 		@visualizzazioni = Siteview.count
-	end
-
-	def https
-		response.set_header('X_FORWARDED_PROTO', 'https')
-		return nil if Rails.env.development?
-		return redirect_to(request.original_url.gsub("http://www.piccolidiavoli.it","https://www.piccolidiavoli.it")) if request.original_url["http://"]
 	end
 
 	def set_title_and_description
